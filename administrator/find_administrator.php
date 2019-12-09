aaca512ee51068922a4a0f423e5d271c77883071
Your Name <a.selaudin@gmail.com>
2019-12-09 19:17:41 +0100

    find_administrator changed to POST method

--- administrator/find_administrator.php
+++ administrator/find_administrator.php
@@ -1,45 +1,49 @@
 <?php
 
-    // Headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST, GET');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-    include_once '../config/Database.php';
-    include_once '../models/Administrator.php';
-
-
-    // Instantiate DB & connect
-    $database = new Database();
-    $db = $database->connect();
-
-    $administrator = new Administrator($db);
+include_once '../config/Database.php';
+include_once '../models/Administrator.php';
 
 
-    // get the parameter
-    $username = isset($_GET['username']) ? $_GET['username'] : die();
-    $password = isset($_GET['password']) ? $_GET['password'] : die();
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+$administrator = new Administrator($db);
 
 
-    $result = $administrator->readAdmin($username, $password);
+// get the parameter
+// $username = isset($_POST['username']) ? $_POST['username'] : die();
+// $password = isset($_POST['password']) ? $_POST['password'] : die();
 
-    if ($result->rowCount() != 0) {
-        // Post array
-        $admin_array = array();
+/* $administrator->username=data*/
+$data = json_decode(file_get_contents("php://input"));
 
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            extract($row);
-            $admin_item = array(
-                'name' => $name,
-                'username' => $username,
-                'password' => $password
-            );
+$result = $administrator->readAdmin($data->username, $data->password);
 
-            // Push
-            array_push($admin_array, $admin_item);
-        }
+if ($result->rowCount() != 0) {
+    // Post array
+    $admin_array = array();
 
-        // turn to json output
-        echo json_encode($admin_array);
-    } else {
-        echo json_encode(array());
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $admin_item = array(
+            'name' => $name,
+            'username' => $username,
+            'password' => $password
+        );
+
+        // Push
+        array_push($admin_array, $admin_item);
     }
+
+    // turn to json output
+    echo json_encode($admin_array);
+} else {
+    echo json_encode(array());
+}
