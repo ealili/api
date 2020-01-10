63174b207ef126ce2d3058bf4947b4d3821bbe26
Selaudin Agolli <a.selaudin@gmail.com>
2020-01-10 01:47:09 +0100

    edit phone api created

--- models/Phone.php
+++ models/Phone.php
@@ -134,4 +134,64 @@
 
             return false;
         }
+
+
+        // Edit Phone
+        public function edit()
+        {
+            // Create query
+            $query = 'Update phone SET displayType = :displayType, 
+            displaySize = :displaySize, selfieCamera = :selfieCamera, displayResolution = :displayResolution, mainCamera = :mainCamera,
+            mname = :mname, name = :name, technology = :technology, weight = :weight, sound = :sound, 
+            os = :os, productionYear = :productionYear, battery = :battery, imgSource = :imgSource WHERE id = 
+            :id';
+
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Clean data
+            $this->id = htmlspecialchars(strip_tags($this->id));
+            $this->displayType = htmlspecialchars(strip_tags($this->displayType));
+            $this->displayResolution = htmlspecialchars(strip_tags($this->displayResolution));
+            $this->displaySize = htmlspecialchars(strip_tags($this->displaySize));
+            $this->selfieCamera = htmlspecialchars(strip_tags($this->selfieCamera));
+            $this->mainCamera = htmlspecialchars(strip_tags($this->mainCamera));
+            $this->mname = htmlspecialchars(strip_tags($this->mname));
+            $this->name = htmlspecialchars(strip_tags($this->name));
+            $this->technology = htmlspecialchars(strip_tags($this->technology));
+            $this->weight = htmlspecialchars(strip_tags($this->weight));
+            $this->sound = htmlspecialchars(strip_tags($this->sound));
+            $this->os = htmlspecialchars(strip_tags($this->os));
+            $this->productionYear = htmlspecialchars(strip_tags($this->productionYear));
+            $this->battery = htmlspecialchars(strip_tags($this->battery));
+            $this->imgSource = htmlspecialchars(strip_tags($this->imgSource));
+
+
+            // Bind data
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':displayType', $this->displayType);
+            $stmt->bindParam(':displayResolution', $this->displayResolution);
+            $stmt->bindParam(':displaySize', $this->displaySize);
+            $stmt->bindParam(':selfieCamera', $this->selfieCamera);
+            $stmt->bindParam(':mainCamera', $this->mainCamera);
+            $stmt->bindParam(':mname', $this->mname);
+            $stmt->bindParam(':name', $this->name);
+            $stmt->bindParam(':technology', $this->technology);
+            $stmt->bindParam(':weight', $this->weight);
+            $stmt->bindParam(':sound', $this->sound);
+            $stmt->bindParam(':os', $this->os);
+            $stmt->bindParam(':productionYear', $this->productionYear);
+            $stmt->bindParam(':battery', $this->battery);
+            $stmt->bindParam(':imgSource', $this->imgSource);
+
+            // Execute query
+            if ($stmt->execute()) {
+                return true;
+            }
+
+            // Print error if something goes wrong
+            printf("Error: %s.\n", $stmt->error);
+
+            return false;
+        }
     }
